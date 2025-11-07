2cc12d15cffb54219a7efbaa4db1e76bb4e30ff3
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-11-07 13:25:12 -0600

    Componente de validacion

---
+++ src/lib/components/validacion.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let valido=false;
+    export let descripcion='';
+</script>
+
+{#if valido}
+    <div class="valid-feedback">{descripcion}</div>
+{:else}
+    <div class="invalid-feedback">{descripcion}</div>
+{/if}
