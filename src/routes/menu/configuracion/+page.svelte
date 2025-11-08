9d4ce19740c481efb443798d2a63b95c108435f5
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-11-08 15:36:21 -0600

    Logos y pesonalizacion del boton acerca de la app

--- src/routes/menu/configuracion/+page.svelte
+++ src/routes/menu/configuracion/+page.svelte
@@ -1,34 +1,96 @@
 <script>
     import Nav from '$lib/components/navbar.svelte'
-    
+
+    let mostrarModal=false;
+
+    function abrirModal() {
+        if(mostrarModal){
+            mostrarModal=false;
+            return
+        }
+
+        mostrarModal=true;
+    }
 </script>
 
+<div class="container" style="margin-bottom: 90px;">
+    <h1 class="display-5 text-center mt-5 mt-md-0 ">Configuración</h1>
 
-<div class="container" style="margin-top: 70px; margin-bottom: 60px;">
-    <h1 class="display-5 text-center mb-3">Configuración</h1>
+    <div class="row">
+        
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <button class="btn">
+                <span><i class="bi bi-person" style="font-size: 70px;"></i></span>
+                <p>Perfil</p>
+            </button>
+        </div>
+        
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <button class="btn" on:click={abrirModal}>
+                <span>
+                    <img src="/imgs/logo.png" alt="Logo" style="width: 100px; height: auto;">
+                    <p>Acerca De ContacApp</p>
+                </span>
+            </button>
+        </div>
 
-    <div class="col-12 mt-4 d-flex justify-content-center align-items-center">
-        <button class="btn">
-            <span><i class="bi bi-person" style="font-size: 70px;"></i></span>
-            <p>Perfil</p>
-        </button>
-    </div>
-    
-    <div class="col-12 d-flex justify-content-center align-items-center mt-4">
-        <button class="btn">
-            <span>
-                <img src="/imgs/logo.png" alt="Logo" style="width: 100px; height: auto;">
-                <p>Acerca De ContacApp</p>
-            </span>
-        </button>
+        {#if mostrarModal}
+            <div class="modal fade show" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalCenterTitle" style="display: block;" aria-modal="true" role="dialog">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Acerca De ContacApp</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" on:click={abrirModal}></button>
+                        </div>
+                        <div class="modal-body">
+                            <h4 class="text-center">Bienvenido a contacAPP version 1.0.0 👋</h4>
+
+                            <p>Este proyecto fue realizado por un estudiante de ingenieria en sistemas 3 semestre, lo cual sirvio aprobar una unidad de la materia topicos avanzados de programación.</p>
+                            <p>El proyecto consiste en un CRUD basicos <mark>CREAR, LEER, ACTUALIZAR Y ELIMINAR</mark></p>
+
+                            <p>Tecnologias Utilizadas: </p>
+
+                            <div class="d-flex justify-content-center align-items-center">
+                                <img src="/imgs/logo spring boot.png" alt="logo spring boot" style="width: auto; height: 30px;">
+                            </div>
+
+                            <div class="d-flex justify-content-center align-items-center">
+                                <img src="/imgs/logo sveltekit.png" alt="logo spring boot" style="width: auto; height: 30px;">
+                            </div>
+
+                            <div class="d-flex justify-content-center align-items-center">
+                                <img src="/imgs/logo bootstrap 2.png" alt="logo spring boot" style="width: auto; height: 50px;">
+                            </div>
+
+                            <div class="d-flex justify-content-center align-items-center">
+                                <img src="/imgs/logo mysql.png" alt="logo spring boot" style="width: auto; height: 40px;">
+                            </div>
+
+                            <a href="https://github.com/AlfreGood20/API-AGENDA-CONTACTOS">Ir a resitorio api rest spring boot</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        {/if}
+        
+
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <button class="btn">
+                <span><i class="bi bi-box-arrow-right" style="font-size: 70px; color: red;"></i></span>
+                <p>Cerrar Sesión</p>
+            </button>
+        </div>
+
     </div>
 </div>
 
 <a 
     class="btn btn-link d-flex justify-content-center align-items-center" 
-    style="bottom: 110px; position: fixed; right: 20px;"
+    style="bottom: 90px; position: fixed; right: 20px;"
 
     href="https://github.com/AlfreGood20/PWA-AGENDA-DE-CONTACTO">©copyright
 </a>
+
+
 
 <Nav />
