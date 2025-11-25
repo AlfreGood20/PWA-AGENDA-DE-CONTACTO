a297375946d69263e6013df64b43144cf823083a
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-11-24 20:30:38 -0600

    Arreglado con su validacion

--- src/routes/menu/contactos/nuevo/+page.svelte
+++ src/routes/menu/contactos/nuevo/+page.svelte
@@ -4,9 +4,9 @@
     import { generarAvatar } from '$lib/js/funciones';
     import { contacto } from '$lib/js/clases';
     import { postContacto } from '$lib/api';
+    import Validacion from '$lib/components/validacion.svelte';
 
-
-    let alertaGuardar=false;
+   
     let nombre='';
     let apellidos='';
     let telefono='';
@@ -15,6 +15,15 @@
     let categoria='';
     let colorSeleccionado='';
     
+    let alertaGuardar=false;
+    function accionGuardarContacto() {
+        registrarContacto();
+        alertaGuardar=true;
+        setTimeout(()=>{
+            alertaGuardar=false;
+        },3000);
+    }
+
     /**
      * @type {HTMLInputElement}
      */
@@ -28,102 +37,108 @@
             console.log(e);
         }
     }
-
-
 </script>
 
 <div class="container" style="margin-bottom: 110px;">
 
     <div class="row">
         
-        <div class="col-9 col-md-10 mt-4">
-            <h1>Datos Contacto</h1>
-        </div>
+        <form on:submit={accionGuardarContacto}>
 
-        <div class="col-3 col-md-2 mt-4">
-            <button class="btn btn-link" aria-label="Guardar contacto" title="Guardar contacto" on:click={registrarContacto}>
-                <span><i class="bi bi-save" style="font-size: 45px;"></i></span>
-            </button>
-        </div>
-
-        <Alert color="success" mensaje="Registrado con exito" visible={alertaGuardar}/>
-
-        <div class="col-12 d-flex justify-content-center"> 
-            <input type="color" bind:value={colorSeleccionado} class="form-control" style="display: none;" bind:this={colorInput}>
-            <span>
-                <img src={generarAvatar(nombre, 150, colorSeleccionado, "ffffff")} style="border-radius: 50%; cursor: pointer;" on:click={() => colorInput.click()}>
-            </span>
-        </div>
-        
-        <div class="col-12 mt-4">
-            <p>Nombre del contacto</p>
-        </div>
-
-        <div class="col-12">
-            <div class="input-group">
-                <span class="input-group-text">
-                    <i class="bi bi-person-circle" style="font-size: 1.5rem;"></i>
-                </span>
-                <input bind:value={nombre} placeholder="Nombre" class="form-control" style="height: 60px;">
+            <div class="col-12 mt-5">
+                
+                 <div class="d-flex justify-content-between">
+                    <h1>Datos Contacto</h1>
+                    <button class="btn btn-link p-0" aria-label="Guardar contacto" type="submit" title="Guardar contacto">
+                        <i class="bi bi-save" style="font-size: 45px;"></i>
+                    </button>
+                 </div>
+              
             </div>
-        </div>
-        
-        <div class="col-12 mt-2">
-            <input bind:value={apellidos} placeholder="apellidos" class="form-control" style="height: 60px;">
-        </div>
 
-        <div class="col-12 mt-3">
-            <p>Contactos</p>
-        </div>
-        
-        <div class="col-12">
-            <div class="input-group">
-                <span class="input-group-text">
-                    <i class="bi bi-telephone" style="font-size: 1.5rem;"></i>
-                </span>
-                <input bind:value={telefono} placeholder="+52 99-..." type="number" class="form-control" style="height: 60px;">
+            <Alert color="success" mensaje="Registrado con exito" visible={alertaGuardar}/>
+
+            <div class="col-12 d-flex justify-content-center"> 
+                <input type="color" bind:value={colorSeleccionado} class="form-control" style="display: none;" bind:this={colorInput}>
+                <button type="button" class="btn p-0" aria-label="Seleccionar color" on:click={() => colorInput.click()} style="border: none; background: transparent;">
+                    <img src={generarAvatar(nombre, 150, colorSeleccionado, "ffffff")} alt="avatar" style="border-radius: 50%; cursor: pointer;">
+                </button>
             </div>
-        </div>
+            
+            <div class="col-12 mt-4">
+                <p>Nombre del contacto</p>
+            </div>
+
+            <div class="col-12">
+                <div class="input-group">
+                    <span class="input-group-text">
+                        <i class="bi bi-person-circle" style="font-size: 1.5rem;"></i>
+                    </span>
+                    <input bind:value={nombre} placeholder="Nombre" class="form-control" style="height: 60px;">
+                </div>
+            </div>
+            
+            <div class="col-12 mt-2">
+                <input bind:value={apellidos} placeholder="apellidos" class="form-control" style="height: 60px;">
+            </div>
+
+            <div class="col-12 mt-3">
+                <p>Contactos</p>
+            </div>
+            
+            <div class="col-12">
+                <div class="input-group">
+                    <span class="input-group-text">
+                        <i class="bi bi-telephone" style="font-size: 1.5rem;"></i>
+                    </span>
+                    <input bind:value={telefono} placeholder="+52 99-..." type="number" class="form-control" style="height: 60px;">
+                </div>
+            </div>
+
+            
+            <div class="col-12 mt-3">
+                <div class="input-group">
+                    <span class="input-group-text">
+                        <i class="bi bi-envelope" style="font-size: 1.5rem;"></i>
+                    </span>
+                    <input bind:value={correo} type="email" placeholder="correo@..." class="form-control" style="height: 60px;">
+                </div>
+            </div>
+
+            
+            <div class="col-12 mt-3">
+                <p>Dirección</p>
+            </div>
+            <div class="col-12">
+                <input bind:value={direccion} placeholder="Calle..." class="form-control" style="height: 60px;">
+            </div>
+
+
+            <div class="col-12 mt-3">
+                <p>Categoria</p>
+            </div>
+
+            <div class="col-12">
+                <div class="input-group">
+                    <span class="input-group-text">
+                        <i class="bi bi-grid" style="font-size: 1.5rem;"></i>
+                    </span>
+                    <select bind:value={categoria} class="form-select" style="height: 60px;" required>
+                        <option value="Familia">Familiar</option>
+                        <option value="Escuela">Escuela</option>
+                        <option value="Amigo">Amigo</option>
+                        <option value="Negocio">Negocio</option>
+                        <option value="Novia">Novia</option>
+                        <option value="Novio">Novio</option>
+                        <option value="Ex">Ex</option>
+                        <option value="Amante">Amante</option>
+                    </select>
+                </div>
+            </div>
+
+        </form>
 
         
-        <div class="col-12 mt-3">
-            <div class="input-group">
-                <span class="input-group-text">
-                    <i class="bi bi-envelope" style="font-size: 1.5rem;"></i>
-                </span>
-                <input bind:value={correo} type="email" placeholder="correo@..." class="form-control" style="height: 60px;">
-            </div>
-        </div>
-
-        
-        <div class="col-12 mt-3">
-            <p>Dirección</p>
-        </div>
-        <div class="col-12">
-            <input bind:value={direccion} placeholder="Calle..." class="form-control" style="height: 60px;">
-        </div>
-
-
-        <div class="col-12 mt-3">
-            <p>Categoria</p>
-        </div>
-        <div class="col-12">
-            <div class="input-group">
-                <span class="input-group-text">
-                    <i class="bi bi-grid" style="font-size: 1.5rem;"></i>
-                </span>
-                <select bind:value={categoria} class="form-select" style="height: 60px;">
-                    <option value="Familiar">Familia</option>
-                    <option value="Ecuela">Escuela</option>
-                    <option value="Amigo">Amigo</option>
-                    <option value="Negocio">Negocio</option>
-                    <option value="Novia">Novia</option>
-                    <option value="Novio">Novio</option>
-                    <option value="Ex">Ex</option>
-                    <option value="Amante">Amante</option>
-                </select>
-            </div>
-        </div>
 
     </div>
 
